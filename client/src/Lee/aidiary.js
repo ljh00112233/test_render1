0ab17e6c7bd67992d13b0d6342b14fe7cb67a6f2
ljh00112233 <l268728@naver.com>
2025-04-11 15:01:06 +0900

    Update aidiary.js

--- client/src/Lee/aidiary.js
+++ client/src/Lee/aidiary.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 
 function aidiary() {
+  const navigate = useNavigate();
   const [title, setTitle] = useState("");
   const [keywords, setKeywords] = useState([""]);
   const [diary, setDiary] = useState("");
