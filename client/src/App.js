ab0f8ff0122de9ca0414dae50e8be7ff8a68e70b
ljh00112233 <l268728@naver.com>
2025-04-11 16:05:45 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { Route, BrowserRouter, Routes } from "react-router-dom";
 import MainPage from "./MainPage";
 import AiDiary from "./Lee/aidiary";
 import HandDiary from "./Lee/handdiary";
+import { EnvProvider } from "./Lee/envContext";
 
 const App = () => { //라우터 설정
   return (
