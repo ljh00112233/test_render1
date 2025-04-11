f2647d564baffdc3f5335288ab21212b647b8327
ljh00112233 <l268728@naver.com>
2025-04-11 15:10:23 +0900

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import MainPage from "./MainPage";
 import AiDiary from "./Lee/aidiary";
+import HandDiary from "./Lee/handdiary";
 
 const App = () => { //라우터 설정
   return (
@@ -8,6 +9,7 @@ const App = () => { //라우터 설정
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/handdiary" element={<HandDiary />} />
           <Route path="/aidiary" element={<AiDiary />} />
         </Routes>
       </BrowserRouter>
